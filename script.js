fb4009a625cfd5175a1ec97182ce24f9f0efdebd
Haodong Wu <wuhaodong@Haodong-Wu.local>
2024-10-19 23:37:21 -0700

    change some useless code

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const choices = ['rock', 'paper', 'scissors'];
-const userChoiceElement = document.getElementById('user-choice');
-const computerChoiceElement = document.getElementById('computer-choice');
 const gameResultElement = document.getElementById('game-result');
 
 function playGame(userChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-    // userChoiceElement.textContent = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
-    // computerChoiceElement.textContent = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-    
+   
     document.getElementById('your-choice-icon').innerHTML = `<img src="${userChoice}.png" alt="${userChoice}" class="small-icon">`;
     document.getElementById('computer-choice-icon').innerHTML = `<img src="${computerChoice}.png" alt="${computerChoice}" class="small-icon">`;
 
